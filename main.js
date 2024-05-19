92503e0cd3d2f7b926661edc489609ff1d995760
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2024-05-19 19:14:20 -0300

    222

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const tempoObjetivo2 = new Date("2024-07-30T00:00:00");
 const tempoObjetivo3 = new Date("2024-06-30T00:00:00");
 
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3];
 
 
 function calculaTempo(tempoObjetivo) {
